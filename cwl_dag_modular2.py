78a910a23129fe1d2178d754b4f24189099e1caf
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2026-03-17 11:55:03 -0700

    edited DAG for testing

--- cwl_dag_modular2.py
+++ cwl_dag_modular2.py
@@ -87,19 +87,19 @@ dag = DAG(
         "stac_json": Param(
             DEFAULT_STAC_JSON,
             type="string",
-            title="STAC JSON",
+            title="STAC JSON1",
             description="STAC JSON data to download granules encoded as a JSON string or the URL of a JSON or YAML file",
         ),
         "process_workflow": Param(
             DEFAULT_PROCESS_WORKFLOW,
             type="string",
-            title="Processing workflow",
+            title="Processing workflow1",
             description="The processing workflow URL",
         ),
         "process_args": Param(
             DEFAULT_PROCESS_ARGS,
             type="string",
-            title="Processing workflow parameters",
+            title="Processing workflow parameters1",
             description=(
                 "The processing job parameters encoded as a JSON string," "or the URL of a JSON or YAML file"
             ),
